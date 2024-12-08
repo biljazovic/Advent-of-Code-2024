edaa2c4df0a2e922ceb35d6bd6a852994097b33e
Bruno Iljazovic <bruno.iljazovic@gmail.com>
2024-12-08 13:13:12 +0100

    day 8

---
+++ src/Day08.hs
@@ -0,0 +1,31 @@
+module Day08 (main08) where
+
+import Util
+import qualified Data.Map as Map
+import qualified Data.Array as Arr
+
+solve nodes mat =
+  mat & Arr.assocs
+      & filter ((/= '.') . snd)
+      & map (\(x, y) -> (y, [x]))
+      & Map.fromListWith (++)
+      & Map.assocs
+      & concatMap (snd >>> makePairs >>> concatMap (nodes mat))
+      & mkUniq
+      & length
+
+nodesA mat (x, y) = filter (inBounds (Arr.bounds mat)) [ x - (y - x), y - (x - y) ]
+
+nodesB mat (x, y) = f y dir ++ f x (-dir)
+  where
+    V2 dx' dy' = y - x
+    g = gcd dx' dy'
+    dir = V2 (dx' `div` g) (dy' `div` g)
+    f start dir = takeWhile (inBounds (Arr.bounds mat)) $ iterate (+dir) start
+
+
+main08 :: IO ()
+main08 = do
+    input <- parseMatrix <$> readFile "res/input08"
+    print $ solve nodesA input
+    print $ solve nodesB input
